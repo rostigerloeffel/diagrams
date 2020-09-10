f4341ef6ee2cc6efbf181e40a0ead1b4ef4f3f96
Robert Stelzmann <robert.stelzmann@isax.com>
2020-09-10 14:01:09 +0200

    Declare createLine

--- packages/diagram-js/lib/util.d.ts
+++ packages/diagram-js/lib/util.d.ts
@@ -1,2 +1,7 @@
+declare module 'diagram-js/lib/util'
+
 declare module 'diagram-js/lib/util/Text'
-declare module 'diagram-js/lib/util/RenderUtil'
+
+declare module 'diagram-js/lib/util/RenderUtil' {
+  export function createLine(waypoints: any, attrs: any): any
+}
