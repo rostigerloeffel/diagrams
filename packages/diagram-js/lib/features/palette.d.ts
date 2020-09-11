e4462b2a0fa63423707db951bb172c99bd6716dd
Robert Stelzmann <robert.stelzmann@isax.com>
2020-09-11 16:33:24 +0200

    Declare palette

--- packages/diagram-js/lib/features/palette.d.ts
+++ packages/diagram-js/lib/features/palette.d.ts
@@ -1,1 +1,20 @@
-declare module 'diagram-js/lib/features/palette'
+declare module 'diagram-js/lib/features/palette' {
+  import { Module } from 'didi'
+
+  const module: Module
+  export default module
+}
+
+declare module 'diagram-js/lib/features/palette/Palette' {
+  import { Canvas, EventBus } from 'diagram-js/lib/core'
+
+  export interface PaletteProvider {
+    getPaletteEntries(): object
+  }
+
+  export default class Palette {
+    constructor(eventBus: EventBus, canvas: Canvas)
+    registerProvider(priority: number, provider: PaletteProvider)
+    registerProvider(provider: PaletteProvider)
+  }
+}
