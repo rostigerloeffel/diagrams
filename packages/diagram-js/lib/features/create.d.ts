77392f340a323894aa8d1a04732a7aad508d716b
Robert Stelzmann <robert.stelzmann@isax.com>
2020-09-22 09:29:20 +0200

    Fix 'create' declaration

--- packages/diagram-js/lib/features/create.d.ts
+++ packages/diagram-js/lib/features/create.d.ts
@@ -8,37 +8,39 @@ declare module 'diagram-js/lib/features/create' {
 }
 
 declare module 'diagram-js/lib/features/create/Create' {
+  import { Base } from 'diagram-js/lib/model'
   import { Canvas, EventBus } from 'diagram-js/lib/core'
   import { Modeling } from 'diagram-js/lib/features/modeling'
 
   export interface Create {
-    start(event: string | object, elements: Base | Base[], context: object)
-    start(event: string | object, elements: Base | Base[])
+    start(event: string | object, elements: Base | Base[], context: object): void
+    start(event: string | object, elements: Base | Base[]): void
   }
 
   export default class DefaultCreate {
     constructor(canvas: Canvas, dragging: any, eventBus: EventBus, modeling: Modeling, rules: any)
 
-    start(event: string | object, elements: Base | Base[], context: object)
-    start(event: string | object, elements: Base | Base[])
+    start(event: string | object, elements: Base | Base[], context: object): void
+    start(event: string | object, elements: Base | Base[]): void
 
-    protected canCreate(elements: Base[], target: Base, position: Point, source: Base, hints: boolean | null | object)
-    protected setMarker(element: Base | string, marker: string)
-    protected cancel()
+    protected canCreate(elements: Base[], target: Base, position: Point, source: Base, hints: boolean | null | object): void
+    protected setMarker(element: Base | string, marker: string): void
+    protected cancel(): void
   }
 }
 
 declare module 'diagram-js/lib/features/create/CreatePreview' {
+  import { Base } from 'diagram-js/lib/model'
   import { Canvas, EventBus } from 'diagram-js/lib/core'
 
   export interface CreatePreview {
-    createDragGroup(elements: Base[])
+    createDragGroup(elements: Base[]): void
   }
 
   export default class DefaultCreatePreview implements CreatePreview {
     constructor(canvas: Canvas, eventBus: EventBus, graphicsFactory: any, previewSupport: any, styles: any)
 
-    createDragGroup(elements: Base[])
+    createDragGroup(elements: Base[]): void
   }
 }
 
